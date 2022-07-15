a2d284641439af330f1d141c8b227909540a2e58
Tyler Mills <tyler@garlik.io>
2022-07-15 13:10:35 -0400

    Refactor static-versioned.php to use Magento's static.php

--- pub/static.php
+++ pub/static.php
@@ -6,6 +6,20 @@
  * See COPYING.txt for license details.
  */
 
+/**
+ * Begin Platform.sh Patch
+ * @deprecated this can be handled in .platform.app.yaml:locations
+ */
+$parts = explode('/', $_SERVER['REQUEST_URI']);
+// $parts is ['', 'static', 'version<\d+>', ...], and only '...' is
+// what we're interested in.
+array_shift($parts);
+array_shift($parts);
+array_shift($parts);
+
+$_GET['resource'] = implode('/', $parts);
+/** End Platform.sh Patch */
+
 require realpath(__DIR__) . '/../app/bootstrap.php';
 $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
 /** @var \Magento\Framework\App\StaticResource $app */
